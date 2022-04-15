5cb0df97fdb7b1ac1b6fe4162f824662c3b1ad51
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-15 16:38:18 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+function Zasifruj (text: string, posun: number) {
+    for (let i = 0; i <= text.length - 1; i++) {
+        konecnyText = "" + konecnyText + cipher.encryptCharacter(text.charAt(i), posun)
+    }
+    return konecnyText
+}
+function Desifruj (text: string, posun: number) {
+    for (let j = 0; j <= text.length - 1; j++) {
+        konecnyText = "" + konecnyText + cipher.decryptCharacter(text.charAt(j), posun)
+    }
+    return konecnyText
+}
+let konecnyText = ""
+basic.showString("" + (Zasifruj("microbit", 5)))
